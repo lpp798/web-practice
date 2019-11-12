010e2879a7b22d75d4acf212e8ca7d3ba0f33122
lpp798 <2458880391@qq.com>
2019-11-12 14:53:59 +0800

    san数据双向绑定

--- san-data-manipulation/src/index.js
+++ san-data-manipulation/src/index.js
@@ -9,6 +9,7 @@ var MyApp = San.defineComponent({
         <input value="{= name =}" placeholder="姓名（string）"/>
         <input value="{= age =}" placeholder="年龄（number）"/>
         <input value="{= des =}" placeholder="简介（string）"/>
+        <div>信息：<button type="button" on-click="removeMsg">移除信息</button></div>
         <div class="display">
             <p>姓名：{{name}}</p>
             <p>年龄：{{age}}</p>
@@ -22,6 +23,11 @@ var MyApp = San.defineComponent({
             age: null,
             des: ''
         }
+    },
+    removeMsg: function(){
+        this.data.set('name','');
+        this.data.set('age',null);
+        this.data.set('des','');
     }
 });
 
